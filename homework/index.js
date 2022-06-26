206ca591c674c4a450683e1c849eb537abf2ccaa
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-06-26 21:36:42 +0200

    Hide/display items

--- homework/index.js
+++ homework/index.js
@@ -36,7 +36,60 @@
       if (err) {
         createAndAppend('div', root, { text: err.message, class: 'alert-error' });
       } else {
-        createAndAppend('pre', root, { text: JSON.stringify(data, null, 2) });
+        //createAndAppend('pre', root, { text: JSON.stringify(data, null, 2) });
+        const header = createAndAppend("header", root, { class: "header" })
+        const headerText = createAndAppend("h1", header, { class: "headerText", text: "FooCoding Repositories" })
+        const select = createAndAppend("select", header, { class: "select" });
+
+        select.addEventListener("change", (e) => {
+          let dataVar = document.getElementsByClassName("dataDiv")
+
+          for (let x = 0; x < dataVar.length; x++) {
+            const element = dataVar[x];
+            element.style.display = "none";
+          }
+
+          document.getElementById(e.target.value).style.display = "block"
+          //console.log(e.target.value);
+        })
+        const container = createAndAppend("div", root, { class: "container" })
+
+        for (let i = 0; i < data.length; i++) {
+          const element = data[i];
+          const name = element.name;
+          const option = createAndAppend("option", select, { text: name, value: i })
+
+          const dataDiv = createAndAppend("div", container, { class: name, class: "dataDiv", id: i })
+          dataDiv.style.display = "none";
+
+          const repository = element.svn_url;
+          const repo = createAndAppend("a", dataDiv, { class: "repository", text: name, href: repository })
+
+          const descriptions = element.description;
+          const description = createAndAppend("p", dataDiv, { class: "describtion", text: descriptions }) // if(description === null) return display: none
+
+          const forks = element.forks_count;
+          const fork = createAndAppend("p", dataDiv, { class: "fork", text: forks })
+
+          const updated = element.updated_at;
+          const update = createAndAppend("p", dataDiv, { class: "updated", text: updated })
+
+          fetchJSON(element.contributors_url, (error, contributors) => {
+            if (error) {
+
+            } else {
+              const contibutorDiv = createAndAppend("div", dataDiv, { class: "contributors" })
+
+
+              for (let j = 0; j < contributors.length; j++) {
+                const el = contributors[j];
+
+                const avatars = el.avatar_url;
+                const avatar = createAndAppend("img", contibutorDiv, { src: avatars, class: "avatarImg" })
+              }
+            }
+          })
+        }
       }
     });
   }
